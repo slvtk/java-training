06d6ad93086ea5d179b0bc4441372e74d35f98c8
Salavat Nizamov <slwtk@mail.ru>
2020-12-10 16:01:02 +0300

    ASum task added

--- src/main/java/codewars/six/ASum.java
+++ src/main/java/codewars/six/ASum.java
@@ -1,15 +1,12 @@
 package codewars.six;
 
+//because of Math.pow returns double, used multiply pow counting
 public class ASum {
-    public static void main(String[] args) {
-        System.out.println(findNb(1749405741869456784L));
-    }
-
     public static long findNb(long m) {
         long n = 0;
         while (m > 0) {
             n++;
-            m -= Math.pow(n, 3);
+            m -= n * n * n;
         }
         return m == 0 ? n : -1;
     }
